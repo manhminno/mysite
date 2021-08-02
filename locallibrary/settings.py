d93c70aab858dee2063d2319810d1491a4b0c0d7
manh6264@gmail.com <manh6264@gmail.com>
2021-08-02 16:07:31 +0700

    Django Tutorial Part 5: Creating our home page

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
+
+LANGUAGES = (("en-us", _("English")), ("vi", _("Vietnamese")))
 
 TIME_ZONE = "UTC"
 
